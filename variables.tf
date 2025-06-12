7997bad9c8c727ad903a363dfe827ea517d0e643
fareed674 <fareedshareef679@gmail.com>
2025-06-12 14:45:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,10 @@
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
 variable "kubernetes_version" {
-  default     = 1.32
-  description = "kubernetes version"
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.32"
 }
-
-variable "vpc_cidr" {
-  default     = "10.0.0.0/16"
-  description = "default CIDR range of the VPC"
-}
-variable "aws_region" {
-  default = "ap-south-1"
-  description = "aws region"
-}
-
